3a3765b16573e3257a005179b64a3d5351d4833f
Maryam ElOraby <maryam.eloraby@gmail.com>
2022-11-27 22:44:02 +0200

    New branch to save latest

--- src/code/Node.java
+++ src/code/Node.java
@@ -212,6 +212,7 @@ public class Node {
                                 deadPassengers--;
                             }
 
+
                             remainingCapacity -= shipPassengers;
                             shipPassengers = 0;
                             remainingShips--;
