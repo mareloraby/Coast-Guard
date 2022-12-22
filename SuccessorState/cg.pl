c3721e452035598ab31c2da4a2c65be0263bec2f
Maryam ElOraby <maryam.eloraby@gmail.com>
2022-12-22 23:35:24 +0200

    Updated cg.pl

--- SuccessorState/cg.pl
+++ SuccessorState/cg.pl
@@ -1,10 +1,10 @@
-:-include('KB2.pl').
+:-include('KB.pl').
 
 /* Movement Actions*/
 up(A,PrevX,CgY,CgX,CgY,PrevShips,PrevCarriedCount,PrevCarriedCount,PrevShips):-
     CgX >= 0,
     PrevX is CgX+1,
-    A = up.	
+    A = up.
 down(A,PrevX,CgY,CgX,CgY,PrevShips,PrevCarriedCount,PrevCarriedCount,PrevShips):-
     grid(D,D),CgX < D,
     PrevX is CgX-1,
@@ -13,13 +13,13 @@ left(A,CgX,PrevY,CgX,CgY,PrevShips,PrevCarriedCount,PrevCarriedCount,PrevShips):
     CgY >= 0,
     PrevY is CgY+1,
     A = left.
-	
 right(A,CgX,PrevY,CgX,CgY,PrevShips,PrevCarriedCount,PrevCarriedCount,PrevShips):-
     grid(D,D),CgY < D,
     PrevY is CgY-1,
     A = right.
 
-/* This was the last passenger to pickup. The ship location will be added to the previous ships list and removed from current ships list */
+/* This was the last and only passenger to pickup in the grid. The ship location will be added to the previous ships list and removed from current ships list
+    The grid contains only 1 ship*/
 pickup(A,CgX,CgY,CgX,CgY, CurrCarriedCount,[],[[CgX,CgY]],PrevCarriedCount):-
     capacity(C),
     (ships_loc([[CgX,CgY]]);
@@ -29,7 +29,8 @@ pickup(A,CgX,CgY,CgX,CgY, CurrCarriedCount,[],[[CgX,CgY]],PrevCarriedCount):-
     PrevCarriedCount is CurrCarriedCount - 1,
     A = pickup.
 
-/* 1 passenger will be remaning to pickup. The ship's location where its passenger was picked up will be added to the previous ships list and removed from current ships list*/
+/* 1 passenger will be remaning to pickup. The ship's location where its passenger was picked up will be added to the previous ships list and removed from current ships list
+   The grid contains 2 ships*/
 pickup(A,CgX,CgY,CgX,CgY, 1,[Sh2],[[CgX,CgY],Sh2],0):-
     (ships_loc([[CgX,CgY],_]);
     ships_loc([_,[CgX,CgY]])),
@@ -49,7 +50,7 @@ drop(A,CgX,CgY,CgX,CgY, 0,[],[],2):-
 drop(A,CgX,CgY,CgX,CgY, 0,[],[],1):-
     station(CgX,CgY),
     A = drop.
-     
+
 /*Carrying one passenger and the grid had 2 ships.*/
 drop(A,CgX,CgY,CgX,CgY, 0,[Sh1],[Sh1],1):-
     station(CgX,CgY),
@@ -60,12 +61,12 @@ drop(A,CgX,CgY,CgX,CgY, 0,[Sh1],[Sh1],1):-
 /* Base case of cgAction: if coastGuard will need just 1 move to reach the initial position*/
 cgAction(CgX,CgY,PrevShips,0,result(A,s0)):-
     agent_loc(PrevX,PrevY),   /*agent standing at initial position mentioned in the KB*/
-    ships_loc(PrevShips),  
+    ships_loc(PrevShips),
     (down(A,PrevX,PrevY,CgX,CgY,_,_,_,_);
     left(A,PrevX,PrevY,CgX,CgY,_,_,_,_);
     right(A,PrevX,PrevY,CgX,CgY,_,_,_,_);
     up(A,PrevX,PrevY,CgX,CgY,_,_,_,_)).
-	
+
 /* Recursive case of coast guard actions till reaching the above base case */
 cgAction(CgX,CgY,CurrShips,CurrCarriedCount,result(A,S)):-
     (
@@ -77,7 +78,7 @@ cgAction(CgX,CgY,CurrShips,CurrCarriedCount,result(A,S)):-
     drop(A,PrevX,PrevY,CgX,CgY,CurrCarriedCount,CurrShips,PrevShips,PrevCarriedCount)
 	),
     cgAction(PrevX,PrevY,PrevShips,PrevCarriedCount,S).
-	
+
 
 goal(S):-
 	ids(S,1).
